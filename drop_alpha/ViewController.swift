0017db1917b6f068e49bdf26ff1c0bb6563ec86f
Mike Hargrove <mike.hargrove@gmail.com>
2017-09-02 23:16:22 -0500

    Initial Commit

---
+++ drop_alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  drop_alpha
+//
+//  Created by mike hargrove on 9/2/17.
+//  Copyright © 2017 mike hargrove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
